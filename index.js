7ff45f828c16bd62e236191f4c41627cb2952f6d
soumik9 <46122468+soumik9@users.noreply.github.com>
2022-05-18 17:58:12 +0600

    initialize server with database

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@todo.zjxkw.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run(){
+    try{
+
+    }finally{
+
+    }
+}
+
+run().catch(console.dir);
+
+// port listening
+app.listen(port, () => {
+    console.log('Listening to port, ', port);
+})
