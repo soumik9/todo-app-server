477c025e80b260b29c341e8d79d6f9b29c744f74
soumik9 <46122468+soumik9@users.noreply.github.com>
2022-05-19 01:53:57 +0600

    final check

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ app.use(express.json());
 // jwt verification
 function verifyJWT(req, res, next){
     const authHeader = req.headers.authorization;
-   
+
     if(!authHeader){
         return res.status(401).send({message: 'Unauthorized access'});
     }
 
     const token = authHeader.split(' ')[1];
 
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
         if(err){
             return res.status(403).send({message: 'Forbidden access'});
         }
